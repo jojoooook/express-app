6ee489b3ad08c417ad05348bd8a4479edd5945ff
Jonathan <jonathanchristian1221@gmail.com>
2024-10-11 11:22:08 +0700

    MVC ejs

---
+++ app_server/controllers/mainController.js
@@ -0,0 +1,23 @@
+const index = (req,res) => {
+     const berita = [
+        {
+            judul:"Berita 1",
+            isi: "Isi berita 1"
+        },
+        {
+            judul:"Berita 2",
+            isi: "Isi berita 2"
+        },
+    ];
+    res.render('index' , {title : 'Halaman Home', berita, layout : 'main'});
+}
+
+const about = (req,res) => {
+    res.render('about' , {title : 'About Us', layout : 'main' });
+}
+
+const contact = (req,res) => {
+    res.render('Contact' , {title : 'Contact Us', layout : 'main'});
+}
+
+module.exports = {index, about, contact}
